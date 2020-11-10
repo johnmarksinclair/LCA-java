ab482be27b37e8bbfc9422bed23dcb5c19a3d1ba
John Sinclair <sinclairjohn72@gmail.com>
2020-11-10 12:55:20 +0000

    added test case to test all of node.java

--- Node.java
+++ Node.java
@@ -4,22 +4,38 @@ public class Node {
 
 	private char key;
 	private ArrayList<Node> children;
-	
+
 	Node(char key) {
 		this.key = key;
 		this.children = new ArrayList<Node>();
 	}
 
-	Node(char key, ArrayList<Node> kids) {
-		this.key = key;
-		this.children = kids;
-	}
-	
+//	Node(char key, ArrayList<Node> kids) {
+//		this.key = key;
+//		this.children = kids;
+//	}
+
 	public char getKey() {
 		return this.key;
 	}
-	
+
 	public ArrayList<Node> getChildren() {
 		return this.children;
+	}
+
+	public ArrayList<Character> getChildrensKeys() {
+		ArrayList<Character> kidsKeys = new ArrayList<Character>();
+		for (int i = 0; i < this.getChildren().size(); i++) {
+			kidsKeys.add(this.getChildren().get(i).getKey());
+		}
+		return kidsKeys;
+	}
+
+	public void addChild(Node kid) {
+		this.children.add(kid);
+	}
+
+	public void addChildren(ArrayList<Node> kids) {
+		this.children.addAll(kids);
 	}
 }
--- NodeTest.java
+++ NodeTest.java
@@ -1,12 +1,55 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 class NodeTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testConstructor() {
+		Node a = new Node('a');
+		assertTrue(a != null);
+	}
+
+	@Test
+	void testGetKey() {
+		Node a = new Node('a');
+		assertEquals('a', a.getKey());
+	}
+
+	@Test
+	void testGetChildren() {
+		Node a = new Node('a');
+		Node b = new Node('b');
+		Node c = new Node('c');
+		Node d = new Node('d');
+		Node e = new Node('e');
+		a.addChild(b);
+		b.addChild(c);
+		b.addChild(d);
+		c.addChild(a);
+		d.addChild(e);
+		e.addChild(a);
+		ArrayList<Character> bsChildren = new ArrayList<Character>();
+		bsChildren.add('c');
+		bsChildren.add('d');
+		assertEquals(bsChildren, b.getChildrensKeys());
+	}
+	
+	@Test
+	void testAddChildren() {
+		Node a = new Node('a');
+		Node b = new Node('b');
+		Node c = new Node('c');
+		ArrayList<Node> kids = new ArrayList<Node>();
+		kids.add(b);
+		kids.add(c);
+		a.addChildren(kids);
+		ArrayList<Character> asChildren = new ArrayList<Character>();
+		asChildren.add('b');
+		asChildren.add('c');
+		assertEquals(asChildren, a.getChildrensKeys());
 	}
 
 }
