f1214cc06221b7274bbedafffdf32dc270f37a1d
John Sinclair <sinclairjohn72@gmail.com>
2020-11-10 16:10:34 +0000

    finished, possibly add more tests

--- LCA.java
+++ LCA.java
@@ -4,16 +4,26 @@ public class LCA {
 
 	public static ArrayList<Character> ancestors;
 
-	public Node getLCA(Node one, Node two) {
-
-		return null;
+	public static char getLCA(Node one, Node two) {
+		ArrayList<Character> onesAncestors = getAncestorsKeys(one, true);
+		ArrayList<Character> twosAncestors = getAncestorsKeys(two, true);
+//		System.out.println(onesAncestors.toString());
+//		System.out.println(twosAncestors.toString());
+		for (int i = 0; i < twosAncestors.size(); i++) {
+			for (int j = 0; j < onesAncestors.size(); j++) {
+				if (twosAncestors.get(i) == onesAncestors.get(j)) {
+					return twosAncestors.get(i);
+				}
+			}
+		}
+		return '!';
 	}
 
 	public static ArrayList<Character> getAncestorsKeys(Node node, boolean firstPass) {
 		if (firstPass)
 			ancestors = new ArrayList<Character>();
 		for (int i = 0; i < node.getParents().size(); i++) {
-			//System.out.println(node.getParents().get(i).getKey());
+			// System.out.println(node.getParents().get(i).getKey());
 			char temp = node.getParents().get(i).getKey();
 			boolean flag = false;
 			for (int j = 0; j < ancestors.size(); j++) {
@@ -23,7 +33,7 @@ public class LCA {
 			}
 			if (flag == false) {
 				ancestors.add(temp);
-				//System.out.println(ancestors.toString());
+				// System.out.println(ancestors.toString());
 			}
 			getAncestorsKeys(node.getParents().get(i), false);
 		}
--- LCATest.java
+++ LCATest.java
@@ -1,7 +1,5 @@
 import static org.junit.Assert.assertEquals;
 
-import org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 class LCATest {
@@ -20,6 +18,25 @@ class LCATest {
 		e.addParent(c);
 		e.addParent(d);
 		assertEquals("[c, a, d, b]", LCA.getAncestorsKeys(e, true).toString());
+	}
+	
+	@Test
+	void testGetLCA() {
+		Node a = new Node('a');
+		Node b = new Node('b');
+		Node c = new Node('c');
+		Node d = new Node('d');
+		Node e = new Node('e');
+		b.addParent(a);
+		c.addParent(a);
+		d.addParent(b);
+		d.addParent(c);
+		e.addParent(c);
+		e.addParent(d);
+		assertEquals('c', LCA.getLCA(d, e));
+		assertEquals('a', LCA.getLCA(b, e));
+		assertEquals('a', LCA.getLCA(b, c));
+		assertEquals('a', LCA.getLCA(c, d));
 	}
 
 }
